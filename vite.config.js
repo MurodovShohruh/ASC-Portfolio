1ebde173b15aa95a21f83ee081989b1ad7f084d7
MurodovShohruh <murodovshohruh2003@gmail.com>
2026-03-27 13:16:33 +0500

    Build dist

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'  // 3.x versiya uchun
+
 export default defineConfig({
-  base: '/asc-portfolio/',
-  build: {
-    outDir: 'dist', // 👈 bu folder GitHub Actions topishi kerak
-  },
+  base: '/asc-portfolio/',  // GitHub Pages repo nomi
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
-        { src: 'index.html', dest: '' },
+        { src: 'index.html', dest: '' }, // React Router uchun 404 muammosi
       ]
     })
-  ]
+  ],
+  build: {
+    outDir: 'dist' // GitHub Actions workflow shu papkani oladi
+  }
 })
