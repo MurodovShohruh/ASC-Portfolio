d31817c4a2c566be7fc7043d27a921c40ca6abb1
MurodovShohruh <murodovshohruh2003@gmail.com>
2026-03-27 13:14:30 +0500

    Deploy dist to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'  // 👈 3.x uchun named import
-
 export default defineConfig({
-  base: '/asc-portfolio/',  // GitHub Pages repo nomi
+  base: '/asc-portfolio/',
+  build: {
+    outDir: 'dist', // 👈 bu folder GitHub Actions topishi kerak
+  },
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
-        { src: 'index.html', dest: '' }, // React Router uchun 404 muammosi
+        { src: 'index.html', dest: '' },
       ]
     })
   ]
