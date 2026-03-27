31a950c00e6b328e8a656829bdcb18a7b40ee68a
༺ 𒆜ASC𒆜 ༻ <murodovshohruh2003@gmail.com>
2026-03-27 12:57:56 +0500

    Integrate ViteStaticCopy plugin in Vite config
    Added ViteStaticCopy plugin for static file copying.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteStaticCopy from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: "/MurodovShohruh/", // <--- shu qator muhim
-  plugins: [react()],
+  base: '/MurodovShohruh/',
+  plugins: [
+    react(),
+    ViteStaticCopy({
+      targets: [
+        { src: 'index.html', dest: '' },
+      ]
+    })
+  ]
 })
